10acb5880b3151efda5b24292cdc7fb5c87aa235
Urmila8 <kumariullak@gmail.com>
2024-01-03 14:41:38 +0530

    fifth changes

--- main1.py
+++ main1.py
@@ -1,8 +1,8 @@
 def get_sddition(a,b):
     return a+b
 
-def get_subtraction(a,b,c):
-    return a-b-c
+def get_subtraction(a,b,r):
+    return a-b-r
 
 def get_multiplication(a,b):
     return a*b
