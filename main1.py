51bd61af5f0b29b45277cb5dde7947c1ced6d570
Urmila8 <132138073+Urmila8@users.noreply.github.com>
2024-01-03 14:42:17 +0530

    Update main1.py

--- main1.py
+++ main1.py
@@ -1,8 +1,8 @@
 def get_sddition(a,b):
     return a+b
 
-def get_subtraction(a,b,r):
-    return a-b-r
+def get_subtraction(a,b,h):
+    return a-b-h
 
 def get_multiplication(a,b):
     return a*b
